da1408e412085c114cbb5d70cad2f92cd9363371
EgorKudryashov <70726535+EgorKudryashov@users.noreply.github.com>
2021-10-21 15:36:46 +0300

    Create mainPage.jsx

---
+++ src/pages/mainPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Navbar from "../component/UI/Navbar/Navbar";
+import MyButton from "../component/UI/MyButton/MyButton";
+import Card from "../component/UI/Card/Card";
+import {useHistory} from "react-router-dom";
+import '../styles/mainPage.css'
+
+const MainPage = () => {
+    const router = useHistory();
+
+    return (
+        <div>
+            <Navbar/>
+
+            <MyButton onClick={()=>{router.push("/main/audiences")}}> Не жми на меня!</MyButton>
+            <Card style={{marginTop: 50, marginLeft: 400}}  route={()=>{router.push("/main/audiences")}}
+                  image={"https://pr.spbu.ru/images/simvolika/logo/spbu_grey.png"}
+                  text={"Аудитории"}
+            />
+            <Card onClick={()=> true}
+                  image={"https://pr.spbu.ru/images/simvolika/logo/spbu_grey.png"}
+                  text={"Преподаватели"}
+            />
+            <Card onClick={()=> true}
+                  image={"https://pr.spbu.ru/images/simvolika/logo/spbu_grey.png"}
+                  text={"Учебные группы"}
+            />
+            <Card onClick={()=> true}
+                  image={"https://pr.spbu.ru/images/simvolika/logo/spbu_grey.png"}
+                  text={"Дисциплины"}
+            />
+        </div>
+    );
+};
+
+export default MainPage;
