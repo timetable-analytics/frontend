d076bbefa5f8790d3a298b12f63f91e906e815c1
EgorKudryashov <70726535+EgorKudryashov@users.noreply.github.com>
2021-10-21 15:36:48 +0300

    Create audiencePage.jsx

---
+++ src/pages/audiencePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AudiencePage = () => {
+    return (
+        <h1>
+            УШЁЛ ОТСЮДА
+            ЗДЕСЬ ЛЮДИ РАБОТАЮТ
+        </h1>
+    );
+};
+
+export default AudiencePage;
