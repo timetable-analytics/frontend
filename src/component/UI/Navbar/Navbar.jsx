bc221e69a0438032135e0f53a6d9a7f2e544f2ae
Tser9 <75836960+Tser9@users.noreply.github.com>
2021-11-11 00:38:10 +0300

    Возможность перехода на главную страницу

--- src/component/UI/Navbar/Navbar.jsx
+++ src/component/UI/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import classes from "./Navbar.module.css";
 import SideButton from "../sideButton/sideButton";
 import SideNavbar from "../../SideNavbar";
 import {MyLocation} from "../../../routes/MyLocation";
-
+import {useHistory} from "react-router-dom";
 
 const Navbar = () => {
     const [visible, setVisible] = useState(false);
@@ -12,17 +12,19 @@ const Navbar = () => {
 
     let i = MyLocation()
     useEffect( () => setIdPage(page[i])  ,[idPage, i])
-
+    const router = useHistory();
         return (
         <div className={classes.navbar}>
             <SideNavbar visible={visible} setVisible={setVisible}/>
 
             <SideButton onClick={()=> setVisible(true)}/>
 
-            <img style={{marginLeft: 30}} src="https://pr.spbu.ru/images/simvolika/logo/spbu_grey.png"
+            <img style={{marginLeft: 30, cursor: "pointer"}} src="https://pr.spbu.ru/images/simvolika/logo/spbu_grey.png"
                  alt="Логотип Университета"
                  height="60"
                  width="70"
+
+                 onClick={()=>{router.push("/main"); setVisible(false)}}
              />
 
             <h1 className={classes.navbar__link}>
