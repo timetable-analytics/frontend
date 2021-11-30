eda03a61d68e8a860c71d65f1aa2438f428b9ccc
Tser9 <75836960+Tser9@users.noreply.github.com>
2021-11-30 03:15:58 +0300

    Card

--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -2,38 +2,37 @@ import React from 'react';
 import Card from "../component/UI/Card/Card";
 import {useHistory} from "react-router-dom";
 import '../styles/mainPage.css'
-
 const MainPage = () => {
     const router = useHistory();
 
     return (
-        <div>
+        <div >
 
             <Card style={{marginTop: 50, marginLeft: 250}}
                   route={()=>{router.push("/main/audiences")}}
-                  image={"https://pr.spbu.ru/images/simvolika/logo/spbu_grey.png"}
+                  image={"https://img.icons8.com/ios/344/class.png"}
                   text={"Аудитории"}
-                  text1={"info/info/info/info/info \n info/info/info/info/info \n info/info/info/info/info \n info/info/info/info/info \n "}
+                  text1={"• Тип аудитории \n • Здание\n  • Номер аудитории  "}
             />
             <Card style={{marginTop: 0, marginLeft: 250}}
                   route={()=>router.push("/main/teachers")}
                   text={"Преподаватели"}
-                  image={"https://pr.spbu.ru/images/simvolika/logo/spbu_grey.png"}
-                  text1={"info/info/info/info/info \n info/info/info/info/info \n info/info/info/info/info \n info/info/info/info/info \n "}
+                  image={"https://img.icons8.com/ios/344/teacher.png"}
+                  text1={"• Факультет\n • Ф.И.О \n  • Должность "}
 
             />
             <Card style={{marginTop: -500, marginLeft: 800}}
                   route={()=> router.push("/main/student_groups")}
-                  image={"https://pr.spbu.ru/images/simvolika/logo/spbu_grey.png"}
+                  image={"https://img.icons8.com/pastel-glyph/344/business-group.png"}
                   text={"Учебные группы"}
-                  text1={"info/info/info/info/info \n info/info/info/info/info \n info/info/info/info/info \n info/info/info/info/info \n "}
+                  text1={"• Факультет\n • Программа обучения \n  • Номер группы\n  • Курс "}
 
             />
             <Card style={{marginTop: 0, marginLeft: 800}}
                   route={()=> router.push("/main/disciplines")}
-                  image={"https://pr.spbu.ru/images/simvolika/logo/spbu_grey.png"}
+                  image={"https://img.icons8.com/ios/344/school.png"}
                   text={"Дисциплины"}
-                  text1={"info/info/info/info/info \n info/info/info/info/info \n info/info/info/info/info \n info/info/info/info/info \n "}
+                  text1={"info/info/info/info/info \n info/info/info/info/info \n info/info/info/info/info \n info/info/info/info/info  "}
 
             />
         </div>
